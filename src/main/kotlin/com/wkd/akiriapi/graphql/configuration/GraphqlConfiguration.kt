d96f0957aeab1bd49933805fbda58ab583f8e1d2
WKingDeveloper <wkingdeveloper@gmail.com>
2023-02-08 09:13:21 +0900

    feat-new : cors config 설정 추가

--- src/main/kotlin/com/wkd/akiriapi/graphql/configuration/GraphqlConfiguration.kt
+++ src/main/kotlin/com/wkd/akiriapi/graphql/configuration/GraphqlConfiguration.kt
@@ -7,14 +7,21 @@ import graphql.scalars.java.JavaPrimitives.GraphQLLong
 import graphql.scalars.java.JavaPrimitives.GraphQLShort
 import graphql.schema.idl.RuntimeWiring
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class GraphqlConfiguration {
+class GraphqlConfiguration : WebMvcConfigurer {
     @DgsComponent
     class RuntimeWiringRegistration {
         @DgsRuntimeWiring
         fun addScalar(builder: RuntimeWiring.Builder): RuntimeWiring.Builder? {
             return builder.scalar(GraphQLLong).scalar(GraphQLShort).scalar(ExtendedScalars.Json)
         }
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080");
     }
 }
